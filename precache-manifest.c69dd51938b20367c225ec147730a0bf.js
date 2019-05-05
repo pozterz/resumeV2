bb7fa9d5a7fafc3a8d90e5a30ee6877b48213f0c
Pozterz <tharathep@pirsquare.net>
2019-05-05 20:33:40 +0700

    Updates

---
+++ precache-manifest.c69dd51938b20367c225ec147730a0bf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb536afe4e34bdc81af3ad75a1db4c7",
+    "url": "/resumeV2/index.html"
+  },
+  {
+    "revision": "b89e1365bc620c88c257",
+    "url": "/resumeV2/static/css/2.08a4298a.chunk.css"
+  },
+  {
+    "revision": "d9f3ec810da024f732ac",
+    "url": "/resumeV2/static/css/main.59880a09.chunk.css"
+  },
+  {
+    "revision": "b89e1365bc620c88c257",
+    "url": "/resumeV2/static/js/2.3c45f598.chunk.js"
+  },
+  {
+    "revision": "d9f3ec810da024f732ac",
+    "url": "/resumeV2/static/js/main.aab0bf18.chunk.js"
+  },
+  {
+    "revision": "2b00fc7da8ae60342fa3",
+    "url": "/resumeV2/static/js/runtime~main.1fa54dad.js"
+  },
+  {
+    "revision": "eded3ee44f671de06ac1b776ec59c4d1",
+    "url": "/resumeV2/static/media/Go-Logo_Black.eded3ee4.png"
+  },
+  {
+    "revision": "b29324c45ced02ca418e097ae8f52c77",
+    "url": "/resumeV2/static/media/ant-icon.b29324c4.svg"
+  },
+  {
+    "revision": "8e5714e3b89075cee5c1beeb22e9e188",
+    "url": "/resumeV2/static/media/clouds-daylight-heaven-2193239.8e5714e3.jpg"
+  }
+]);
